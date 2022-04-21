5b3e6ab736e422699df7a16d64cf9b08ec6976d0
dimsgg18 <dimasgalangajiwiguna@gmail.com>
2022-04-21 15:00:55 +0700

    menambahkan bagian notif dan edit bagian lain

---
+++ Scripts/home.js
@@ -0,0 +1,10 @@
+function openNav(){
+    document.getElementById("notifMenu").style.display = "block";
+
+
+}
+
+function closeNav(){
+    document.getElementById(`notifMenu`).style.display = `none`;
+
+}
