1aa1c5c9d8c010d59d5a705f843a840d338ea91d
dimsgg18 <dimasgalangajiwiguna@gmail.com>
2022-04-26 20:23:18 +0700

    membetulkan bagian yang sudah di notes

--- Scripts/home.js
+++ Scripts/home.js
@@ -1,3 +1,14 @@
+const notifMenu = document.getElementById(`notifMenu`)
+
+function funcBell(){
+    if(notifMenu.style.display ==`block`){
+        closeNav();
+      }else{
+        openNav();
+    }
+}
+
+
 function openNav(){
     document.getElementById("notifMenu").style.display = "block";
 
